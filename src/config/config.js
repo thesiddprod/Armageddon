1a054b9e7fcc63c05709b8a81b139c3eedabbd05
thesiddprod <thesiddprod@gmail.com>
2025-10-10 23:43:30 +0200

    Test webhook

--- src/config/config.js
+++ src/config/config.js
@@ -37,7 +37,7 @@ export const config = {
       url: "#accueil" 
     },
     {
-      title: "Servicesss",
+      title: "Services",
       url: "#services",
       megaMenu: {
         linkGroups: [
